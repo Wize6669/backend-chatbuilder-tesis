0462d9779d932d2155e75b26138c2de641a875a0
William Zapata <zapataw59@gmail.com>
2026-01-07 00:43:10 -0500

    feat: Add endpoint to retrieve bot configuration by WhatsApp account ID; update CORS origin configuration

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:4200',
+    origin: process.env.FRONTEND_URL ?? 'http://localhost:4200',
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
